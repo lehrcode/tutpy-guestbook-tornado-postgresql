6f7f59ef92c1a99ccdc409a289d2998106d2992d
Tutor <tutor@lehrcode.de>
2024-02-12 20:33:01 +0100

    Database connection pool

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import asyncio
 import logging
 
+import asyncpg
 from tornado.httpserver import HTTPServer
 from tornado.options import define, options, parse_command_line
 from tornado.web import Application
 
 define("port", default=8080, help="port to listen on")
+define("pgurl", default='postgres://postgres:@localhost:5432/postgres', help="PostgreSQL URL")
 
 
 async def main():
+    pool = await asyncpg.create_pool(options.pgurl)
     app = Application()
     server = HTTPServer(app)
     logging.info('Start listening on port %d', options.port)
     server.listen(options.port)
     shutdown_event = asyncio.Event()
     await shutdown_event.wait()
+    await pool.close()
 
 
 if __name__ == '__main__':
